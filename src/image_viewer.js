32961d96a6cd91a77b42569ab9dd09f685edf2a9
Paul WB <higgsbison@gmail.com>
2017-01-14 13:16:54 -0800

    add img and url loader to webpack

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,12 @@
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+
 import './image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = small;
+const bigImage = document.createElement('img');
+bigImage.src = big;
 
 document.body.appendChild(image);
+document.body.appendChild(bigImage);
